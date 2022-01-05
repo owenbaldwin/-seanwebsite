b64cec40c1ffad3f1abb5ae59f7dda636a1cbe87
Owen Baldwin <owenabaldwin@gmail.com>
2022-01-05 14:43:32 +0000

    home banner and navbar fix fonts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
+
+  def cv
+    send_file "app/assets/docs/CV-Sean-Nov-2021.pdf", :type => "application/pdf", :disposition => 'inline'
+  end
+
+
 end
