fc66c4795244b8b16292584425f9a3045b4492d4
Owen Baldwin <owenabaldwin@gmail.com>
2022-12-09 14:30:10 +0000

    yarn rm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,10 @@ class PagesController < ApplicationController
   def portfolio
   end
 
+
   def contact
   end
+
 
   def cv
     send_file "app/assets/docs/CV-Sean-Nov-2021.pdf", :type => "application/pdf", :disposition => 'inline'
