1e24c5a9fb4edc186b1b3900b2671982fb84578a
Owen Baldwin <owenabaldwin@gmail.com>
2022-12-12 18:10:00 +0000

    config webpack env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,32 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack');
 
+
+
+
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
+
+
+
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
 
