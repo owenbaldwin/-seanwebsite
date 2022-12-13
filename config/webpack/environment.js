c9cb604b9bed38db345a7cbb6e3529bb80824953
Owen Baldwin <owenabaldwin@gmail.com>
2022-12-13 12:26:44 +0000

    config webpack env back to normal

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,39 +5,39 @@ const webpack = require('webpack');
 
 
 
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false
-    }
-  }
-};
+// const customConfig = {
+//   resolve: {
+//     fallback: {
+//       dgram: false,
+//       fs: false,
+//       net: false,
+//       tls: false,
+//       child_process: false
+//     }
+//   }
+// };
 
-environment.config.delete('node.dgram')
-environment.config.delete('node.fs')
-environment.config.delete('node.net')
-environment.config.delete('node.tls')
-environment.config.delete('node.child_process')
+// environment.config.delete('node.dgram')
+// environment.config.delete('node.fs')
+// environment.config.delete('node.net')
+// environment.config.delete('node.tls')
+// environment.config.delete('node.child_process')
 
-environment.config.merge(customConfig);
-
-
+// environment.config.merge(customConfig);
 
 
-// // Preventing Babel from transpiling NodeModules packages
-// environment.loaders.delete('nodeModules');
 
-// // Bootstrap 4 has a dependency over jQuery & Popper.js:
-// environment.plugins.prepend('Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery',
-//     jQuery: 'jquery',
-//     Popper: ['popper.js', 'default']
-//   })
-// );
+
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
 
 module.exports = environment
